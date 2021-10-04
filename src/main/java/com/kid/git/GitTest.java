5b58b4631f9ce86d7a54e9afc1d24f8158977e5c
Kid <Kid@jie.com>
2021-10-04 16:15:02 +0800

    first commit

---
+++ src/main/java/com/kid/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.kid.git;
+
+/**
+ * 描述：
+ *
+ * @author 黄志杰
+ * @date 2021/10/4星期一16:12
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
